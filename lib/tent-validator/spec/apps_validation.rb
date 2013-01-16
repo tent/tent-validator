26a5a3131de14a7523fd71055f27cd8253bc6974
Jesse Stuart <jessestuart@gmail.com>
2013-01-15 22:51:24 -0500

    Add GET /apps/:id validation

--- lib/tent-validator/spec/apps_validation.rb
+++ lib/tent-validator/spec/apps_validation.rb
@@ -1,10 +1,20 @@
 module TentValidator
   module Spec
     class AppsValidation < Validation
-      describe "GET /apps" do
+      list_apps = describe "GET /apps" do
         with_client :app, :server => :remote do
           expect_response :tent, :schema => :app, :list => true, :status => 200 do
-            client.app.list
+            res = client.app.list
+            set(:app_id, res.body.first['id'])
+            res
+          end
+        end
+      end
+
+      describe "GET /apps/:id", :depends_on => list_apps do
+        with_client :app, :server => :remote do
+          expect_response :tent, :schema => :app, :status => 200, :properties => { :id => get(:app_id) } do
+            client.app.get(get(:app_id))
           end
         end
       end
