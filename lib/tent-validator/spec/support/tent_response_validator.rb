b313542b0a8ef85c7cfe45df55d1bc5e9d0c03c8
Jesse Stuart <jessestuart@gmail.com>
2013-01-16 16:44:52 -0500

    Fix header expections split pattern

--- lib/tent-validator/spec/support/tent_response_validator.rb
+++ lib/tent-validator/spec/support/tent_response_validator.rb
@@ -19,9 +19,9 @@ module TentValidator
 
       def expect_valid_cors_headers
         expect_header('Access-Control-Allow-Origin', '*')
-        expect_header('Access-Control-Allow-Methods', %w( GET POST HEAD PUT DELETE PATCH OPTIONS ), :split => /[^a-z]+/i)
-        expect_header('Access-Control-Allow-Headers', %w( Content-Type Authorization ), :split => /[^a-z]+/i)
-        expect_header('Access-Control-Expose-Headers', %w( Count Link ), :split => /[^a-z]+/i)
+        expect_header('Access-Control-Allow-Methods', %w( GET POST HEAD PUT DELETE PATCH OPTIONS ), :split => /[^a-z-]+/i)
+        expect_header('Access-Control-Allow-Headers', %w( Content-Type Authorization ), :split => /[^a-z-]+/i)
+        expect_header('Access-Control-Expose-Headers', %w( Count Link ), :split => /[^a-z-]+/i)
       end
     end
   end
