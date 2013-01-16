21305b6424cbe0c0e5e117c2ed7644bf9af93ec2
Jesse Stuart <jessestuart@gmail.com>
2013-01-15 23:09:09 -0500

    Add GET /apps when unauthorized test

--- lib/tent-validator/spec/apps_validation.rb
+++ lib/tent-validator/spec/apps_validation.rb
@@ -1,12 +1,20 @@
 module TentValidator
   module Spec
     class AppsValidation < Validation
-      list_apps = describe "GET /apps" do
+      list_apps = describe "GET /apps (when authorized)" do
         with_client :app, :server => :remote do
           expect_response :tent, :schema => :app, :list => true, :status => 200 do
             res = client.app.list
             set(:app_id, res.body.first['id'])
             res
+          end
+        end
+      end
+
+      describe "GET /apps (when unauthorized)" do
+        with_client :no_auth, :server => :remote do
+          expect_response :tent, :schema => :error, :status => 403 do
+            client.app.list
           end
         end
       end
