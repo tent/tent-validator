25aaa5901ccb9c4685bb76eb3a6e19095a5ce474
Jesse Stuart <jessestuart@gmail.com>
2013-01-15 23:10:26 -0500

    Add explanatory comments to AppsValidation

--- lib/tent-validator/spec/apps_validation.rb
+++ lib/tent-validator/spec/apps_validation.rb
@@ -1,11 +1,18 @@
 module TentValidator
   module Spec
     class AppsValidation < Validation
+      # GET /apps should
+      #   - set CORS headers
+      #   - (when authorized) return a list of apps conforming to the app json schema
+      #   - (when unauthorized) return 403 with a valid json error response
       list_apps = describe "GET /apps (when authorized)" do
         with_client :app, :server => :remote do
           expect_response :tent, :schema => :app, :list => true, :status => 200 do
             res = client.app.list
+
+            # We can assume there is at least one app (this app)
             set(:app_id, res.body.first['id'])
+
             res
           end
         end
@@ -19,6 +26,11 @@ module TentValidator
         end
       end
 
+      # GET /apps/:id should
+      #   - set CORS headers
+      #   - (when authorized and app exists) return app with spcified id conforming to the app json schema
+      #   - (when authorized and app not found) return 404 with a valid json error response
+      #   - (when unauthorized) return 403 with a valid json error response
       describe "GET /apps/:id", :depends_on => list_apps do
         with_client :app, :server => :remote do
           expect_response :tent, :schema => :app, :status => 200, :properties => { :id => get(:app_id) } do
