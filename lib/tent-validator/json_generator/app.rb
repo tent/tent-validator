fe900774f29c5f67ecf43c69030e3b2c3d172a01
Jesse Stuart <jesse@jessestuart.ca>
2013-01-27 16:01:33 -0500

    Fix app json mac_key_id format

--- lib/tent-validator/json_generator/app.rb
+++ lib/tent-validator/json_generator/app.rb
@@ -21,7 +21,7 @@ module TentValidator
     def with_auth
       simple.merge(
         :id => random_id,
-        :mac_key_id => 'a' + random_id,
+        :mac_key_id => 'a:' + random_id,
         :mac_algorithm => 'hmac-sha-256',
         :mac_key => SecureRandom.hex(16)
       )
