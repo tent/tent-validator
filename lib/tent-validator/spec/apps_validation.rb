0dbdbabe2ed4aa4deb1b202d12d8eac08b5c3bf9
Jesse Stuart <jesse@jessestuart.ca>
2013-01-27 13:55:42 -0500

    Update GET /apps/:id validation

--- lib/tent-validator/spec/apps_validation.rb
+++ lib/tent-validator/spec/apps_validation.rb
@@ -74,7 +74,7 @@ module TentValidator
       #   - (when unauthorized) return 404 with a valid json error response
       describe "GET /apps/:id (when authorized via scope)", :depends_on => list_apps do
         with_client :app, :server => :remote do
-          expect_response :tent, :schema => :app, :status => 200, :properties => { :id => get(:app_id) } do
+          expect_response :tent, :schema => :app, :status => 200, :properties => { :id => get(:app_id) }, :excluded_properties => [:mac_key_id, :mac_algorithm, :mac_key] do
             client.app.get(get(:app_id))
           end
         end
