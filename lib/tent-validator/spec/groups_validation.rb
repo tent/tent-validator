4422cdd8acec1205b289c3cb7bc8eae21fae378b
Jesse Stuart <jesse@jessestuart.ca>
2013-01-28 15:39:59 -0500

    Add OPTIONS /groups validation

--- lib/tent-validator/spec/groups_validation.rb
+++ lib/tent-validator/spec/groups_validation.rb
@@ -3,6 +3,12 @@ require 'tentd/core_ext/hash/slice'
 module TentValidator
   module Spec
     class GroupsValidation < Validation
+      describe "OPTIONS /groups" do
+        expect_response :tent_cors, :status => 200 do
+          clients(:app, :server => :remote).http.options("groups")
+        end
+      end
+
       create_authorizations = describe "Create authorizations" do
         # Create app
         app = JSONGenerator.generate(:app, :with_auth)
