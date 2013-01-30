31019562f1551087b766ea54efdf4626850d9b67
Jesse Stuart <jesse@jessestuart.ca>
2013-01-30 11:08:24 -0500

    Fix: let tentd know where it's mounted

--- tent_server.rb
+++ tent_server.rb
@@ -3,11 +3,12 @@ require 'tentd'
 TentValidator.tentd
 class TentServer
   def call(env)
-    match = env['PATH_INFO'] =~ %r{\A/([^/]+)/tent(.*)}
-    if match && (user = TentD::Model::User.first(:public_id => $1))
+    match = env['PATH_INFO'] =~ %r{\A(/([^/]+)/tent)(.*)}
+    if match && (user = TentD::Model::User.first(:public_id => $2))
       TentD::Model::User.current = user
       env['tent.entity'] = user.entity
-      env['PATH_INFO'] = $2
+      env['PATH_INFO'] = $3
+      env['SCRIPT_NAME'] = $1
       TentValidator.tentd.call(env)
     else
       [404, { 'Content-Type' => 'text/plain' }, ['Not Found']]
