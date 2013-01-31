0cc8b711f1aa8b6a1ed4297a7e0604d11434f9ef
Jesse Stuart <jesse@jessestuart.ca>
2013-01-31 11:30:21 -0500

    Don't attempt to run validations with invalid authorization

--- lib/tent-validator/spec.rb
+++ lib/tent-validator/spec.rb
@@ -9,10 +9,17 @@ end
 module TentValidator
   module Spec
     def self.run(&block)
+      return unless verify_authorization
       FollowingsValidation.run(&block)
       ProfileValidation.run(&block)
       GroupsValidation.run(&block)
       AppsValidation.run(&block)
+    end
+
+    def self.verify_authorization
+      client = TentClient.new(TentValidator.remote_server, TentValidator.remote_auth_details)
+      res = client.app.list
+      res.success?
     end
   end
 end
