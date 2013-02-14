bc7ee60f6594ee361b7c3bcf79226991f93733a7
Jesse Stuart <jesse@jessestuart.ca>
2013-02-14 12:40:31 -0500

    Fix ResourceFactory

--- lib/tent-validator/resource_factory.rb
+++ lib/tent-validator/resource_factory.rb
@@ -7,8 +7,13 @@ module TentValidator
     def self.create_resource(example_group, client_options, type, *args)
       data = generate_resource_attributes(type, *args)
       expected_data = slice_expected_data(data.dup)
-      example_group.expect_response(:tent, :schema => type, :status => 200, :properties => expected_data) do
-        example_group.clients(:app, client_options).following.create(data[:entity], data)
+      schema_name = client_options.delete(:schema) || type
+      example_group.expect_response(:tent, :schema => schema_name, :status => 200, :properties => expected_data) do
+        example_group.clients(:app, client_options).send(type).create(data)
+      end
+
+      example_group.expect_response(:tent, :schema => schema_name, :status => 200, :properties => data.slice(:mac_key_id, :mac_key, :mac_algorithm).merge(:groups => (data[:groups] || []).map { |g| g.slice('id') })) do
+        example_group.clients(:app, client_options).send(type).get(data[:id], :secrets => true)
       end
 
       expected_data.inject({}) { |m, (k,v)| m[k.to_s] = v; m }
