d1c90a1745428d3ca7af9e316bf252ec90bcbf74
Jesse Stuart <jesse@jessestuart.ca>
2013-02-15 13:37:40 -0500

    Add post generator options

--- lib/tent-validator/json_generator/post.rb
+++ lib/tent-validator/json_generator/post.rb
@@ -4,13 +4,69 @@ module TentValidator
   class PostJSONGenerator < JSONGenerator
     register :post
 
-    def status(options ={})
+    def base
       {
+        :licenses => 3.times.map { Faker::Internet.url },
+        :published_at => Time.now.to_i,
+        :mentions => [{ :entity => Faker::Internet.url }, { :entity => Faker::Internet.url, :post => 'abc' }]
+      }
+    end
+
+    def status(options ={})
+      base.merge(
         :type => "https://tent.io/types/post/status/v0.1.0",
         :content => {
-          :text => Faker::Lorem.paragraph.slice(0, 256)
+          :text => Faker::Lorem.paragraph.slice(0, 256),
+          :location => {
+            :type => 'Point',
+            :coordinates => [Faker::Address.longitude, Faker::Address.latitude]
+          }
         }
-      }.merge(options)
+      ).merge(options)
+    end
+
+    def essay(options = {})
+      base.merge(
+        :type => "https://tent.io/types/post/essay/v0.1.0",
+        :content => {
+          :title => Faker::Company.catch_phrase,
+          :body => Faker::Lorem.paragraphs(2).join("<br/>"),
+          :except => Faker::Lorem.paragraph,
+          :tags => Faker::Lorem.words(6)
+        }
+      ).merge(options)
+    end
+
+    def photo(options = {})
+      base.merge(
+        :type => "https://tent.io/types/post/photo/v0.1.0",
+        :content => {
+          :caption => Faker::Company.catch_phrase,
+          :albums => [],
+          :tags => Faker::Lorem.words(4),
+          :exif => {}
+        }
+      ).merge(options)
+    end
+
+    def custom(options = {})
+      base.merge(
+        :type => "https://example.org/types/post/custom/v0.1.0",
+        :content => {
+          :foos => {
+            :bar => [1, 2, 3],
+            :baz => ['a', 'b', 'c'],
+            :kips => {
+              :kit => [5,4,3],
+              :klop => [0,1,3]
+            }
+          },
+          :bars => {
+            :candy => ['chewy', 'crunchy'],
+            :soap => ['hard', 'soft']
+          }
+        }
+      ).merge(options)
     end
   end
 end
