6c46db47827d72f8dcc09fb8b84ea2bf68395650
Jesse Stuart <jesse@jessestuart.ca>
2013-03-31 14:18:18 -0400

    Expect schema refs

--- lib/tent-validator/validators/support/tent_schemas.rb
+++ lib/tent-validator/validators/support/tent_schemas.rb
@@ -1,5 +1,5 @@
 require 'tent-schemas'
 
 TentSchemas.schemas.each_pair do |name, schema|
-  TentValidator::Schemas.register(name, schema)
+  TentSchemas.inject_refs!(TentValidator::Schemas.register(name, schema))
 end
