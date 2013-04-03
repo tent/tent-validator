a682e28df4bd91c36c29f4d51ba05de648b88e1a
Jesse Stuart <jesse@jessestuart.ca>
2013-04-03 14:02:04 -0400

    Fix content-type header expectation

--- lib/tent-validator/validators/support/app_post_header_expectation.rb
+++ lib/tent-validator/validators/support/app_post_header_expectation.rb
@@ -1,3 +1,3 @@
 TentValidator::ResponseExpectation::HeaderValidator.register(:app_post, {
-  'Content-Type' => %r{\btype=['"]https://tent\.io/types/app/v0#\S*['"]\b}
+  'Content-Type' => %r{\btype=['"]https://tent\.io/types/app/v0#\S*['"]}
 })
