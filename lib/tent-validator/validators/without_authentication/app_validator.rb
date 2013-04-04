e589854f890190bb0bb93339bda092002d6990ed
Jesse Stuart <jesse@jessestuart.ca>
2013-04-04 12:08:47 -0400

    Add more invalid post content member examples

--- lib/tent-validator/validators/without_authentication/app_validator.rb
+++ lib/tent-validator/validators/without_authentication/app_validator.rb
@@ -26,16 +26,42 @@ module TentValidator
         set(:app_post, data)
       end
 
+      def invalid_value(type, format = nil)
+        case type
+        when "array"
+          Hash.new
+        when "boolean"
+          nil
+        when "number", "integer"
+          "123"
+        when "null"
+          true
+        when "object"
+          ["My parent should be an object!"]
+        when "string"
+          if format
+            case format
+            when 'uri'
+              "I'm not a uri!"
+            end
+          else
+            421
+          end
+        end
+      end
+
       describe "POST /posts" do
         context "without authentication" do
 
           context "when app registration post", :before => :generate_app_post do
+            set(:content_schema, :post_app)
+
             expect_response(:headers => :tent, :status => 200, :schema => :post) do
               data = get(:app_post)
 
               expect_headers(:post)
               expect_properties(data)
-              expect_schema(:post_app, "/content")
+              expect_schema(get(:content_schema), "/content")
 
               res = clients(:no_auth, :server => :remote).post.create(data)
 
@@ -47,7 +73,7 @@ module TentValidator
             end
 
             context "with invalid attributes" do
-              context "when extra field" do
+              context "when extra field in content" do
                 expect_response(:headers => :error, :status => 400, :schema => :error) do
                   data = get(:app_post).dup
                   data[:content][:extra_member] = "I shouldn't be here!"
@@ -55,10 +81,53 @@ module TentValidator
                 end
               end
 
+              invalid_member_expectation = proc do |path, property|
+                expect_response(:headers => :error, :status => 400, :schema => :error) do
+                  data = generate_app_post.dup
+                  pointer = JsonPointer.new(data, path, :symbolize_keys => true)
+                  pointer.value = invalid_value(property['type'], property['format'])
+                  clients(:no_auth, :server => :remote).post.create(data)
+                end
+
+                if property['type'] == 'object' && property['properties']
+                  property['properties'].each_pair do |name, property|
+                    invalid_member_expectation.call(path + "/#{name}", property)
+                  end
+                end
+
+                if property['type'] == 'array' && property['items']
+                  invalid_member_expectation.call(path + "/-", { 'type' => property['items']['type'], 'format' => property['items']['format'] })
+                end
+              end
+
+              context "when content member is wrong type" do
+                TentValidator::Schemas[get(:content_schema)]["properties"].each_pair do |name, property|
+                  invalid_member_expectation.call("/content/#{name}", property)
+                end
+              end
+
               context "when content is wrong type" do
                 expect_response(:headers => :error, :status => 400, :schema => :error) do
                   data = get(:app_post).dup
                   data[:content] = "I should be an object"
+                  clients(:no_auth, :server => :remote).post.create(data)
+                end
+
+                expect_response(:headers => :error, :status => 400, :schema => :error) do
+                  data = get(:app_post).dup
+                  data[:content] = ["My parent should be an object!"]
+                  clients(:no_auth, :server => :remote).post.create(data)
+                end
+
+                expect_response(:headers => :error, :status => 400, :schema => :error) do
+                  data = get(:app_post).dup
+                  data[:content] = nil
+                  clients(:no_auth, :server => :remote).post.create(data)
+                end
+
+                expect_response(:headers => :error, :status => 400, :schema => :error) do
+                  data = get(:app_post).dup
+                  data[:content] = true
                   clients(:no_auth, :server => :remote).post.create(data)
                 end
               end
