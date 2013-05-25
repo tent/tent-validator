485f4c65ae63ebff133d6752ec0cd60a2c8916eb
Jesse Stuart <jesse@jessestuart.ca>
2013-05-25 15:29:55 -0400

    Add post data generators
    - status
    - random (non-standard type)

---
+++ lib/tent-validator/validators/support/post_generators.rb
@@ -0,0 +1,31 @@
+module TentValidator
+  module Support
+    module PostGenerators
+
+      def generate_status_post(is_public=true)
+        {
+          :type => "https://tent.io/types/status/v0#",
+          :content => {
+            :text => "The quick brown fox jumps over the lazy dog."
+          },
+          :permissions => {
+            :public => is_public
+          }
+        }
+      end
+
+      def generate_random_post(is_public=true)
+        {
+          :type => "https://tent.io/types/fictitious/v0#",
+          :content => {
+            :description => "chunky tempeh bacon!"
+          },
+          :permissions => {
+            :public => is_public
+          }
+        }
+      end
+
+    end
+  end
+end
