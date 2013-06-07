e5b15121f755da22df7747b789c8fceee161e3d7
Jesse Stuart <jesse@jessestuart.ca>
2013-06-07 18:44:44 -0400

    Add GET post validations

---
+++ lib/tent-validator/validators/post_singleton_validator.rb
@@ -0,0 +1,120 @@
+module TentValidator
+  class PostSingletonValidator < TentValidator::Spec
+    SetupFailure = Class.new(StandardError)
+
+    require 'tent-validator/validators/support/post_generators'
+    class << self
+      include Support::PostGenerators
+    end
+    include Support::PostGenerators
+
+    require 'tent-validator/validators/support/app_post_generators'
+    include Support::AppPostGenerators
+
+    require 'tent-validator/validators/support/oauth'
+    include Support::OAuth
+
+    create_post = lambda do |opts|
+      data = generate_status_post(opts[:public])
+      res = clients(:app).post.create(data)
+
+      data.delete(:permissions) if opts[:public] == true
+      res_validation = ApiValidator::Json.new(:post => data).validate(res)
+      raise SetupFailure.new("Failed to create post with attachments! #{res.status}\n\t#{Yajl::Encoder.encode(res_validation[:diff])}\n\t#{res.body}") unless res_validation[:valid]
+
+      TentD::Utils::Hash.symbolize_keys(res.body['post'])
+    end
+
+    describe "GET post" do
+      shared_example :get_post do
+        expect_response(:status => 200, :schema => :data) do
+          expect_schema(:post, '/post')
+
+          post = get(:post).dup
+
+          expect_properties(:post => post)
+          expect_properties(:post => { :permissions => property_absent }) unless post.has_key?(:permissions)
+
+          get(:client).post.get(post[:entity], post[:id])
+        end
+      end
+
+      context "when public post" do
+        setup do
+          set(:post, create_post.call(:public => true))
+        end
+
+        context "without auth" do
+          setup do
+            set(:client, clients(:no_auth))
+          end
+
+          behaves_as(:get_post)
+        end
+
+        context "with auth" do
+          context "when post type not authorized" do
+            authenticate_with_permissions(:read_post_types => [])
+
+            behaves_as(:get_post)
+          end
+
+          context "when limited authorization" do
+            authenticate_with_permissions(:read_post_types => %w(https://tent.io/types/status/v0#))
+
+            behaves_as(:get_post)
+          end
+
+          context "when full authorization" do
+            setup do
+              set(:client, clients(:app))
+            end
+
+            behaves_as(:get_post)
+          end
+        end
+      end
+
+      context "when private post" do
+        setup do
+          set(:post, create_post.call(:public => false))
+        end
+
+        context "without auth" do
+          expect_response(:status => 404, :schema => :error) do
+            post = get(:post)
+            clients(:no_auth).post.get(post[:entity], post[:id])
+          end
+        end
+
+        context "with auth" do
+          context "when post type not authorized" do
+            authenticate_with_permissions(:read_post_types => [])
+
+            expect_response(:status => 404, :schema => :error) do
+              post = get(:post)
+              get(:client).post.get(post[:entity], post[:id])
+            end
+          end
+
+          context "when limited authorization" do
+            authenticate_with_permissions(:read_post_types => %w(https://tent.io/types/status/v0#))
+
+            behaves_as(:get_post)
+          end
+
+          context "when full authorization" do
+            setup do
+              set(:client, clients(:app))
+            end
+
+            behaves_as(:get_post)
+          end
+        end
+      end
+    end
+
+  end
+
+  TentValidator.validators << PostSingletonValidator
+end
