62783ddc3905b4a3707b6f33d5ec30fafe2898b8
Jesse Stuart <jesse@jessestuart.ca>
2013-06-16 14:22:06 -0400

    Update subscription validation

--- lib/tent-validator/validators/subscription_validator.rb
+++ lib/tent-validator/validators/subscription_validator.rb
@@ -12,7 +12,7 @@ module TentValidator
         set(:subscription_type_base, %(https://tent.io/types/status/v0))
 
         ##
-        # Setup asyc request expectation for relationship# post
+        # Setup asyc request expectation for relationship#initial post
         expect_async_request(
           :method => "PUT",
           :url => %r{\A#{Regexp.escape(get(:user).entity)}},
@@ -23,56 +23,28 @@ module TentValidator
             'Content-Type' => %r{\brel=['"]#{Regexp.escape("https://tent.io/rels/notification")}['"]}
           )
           expect_headers(
-            'Content-Type' => %r{\A#{Regexp.escape(TentD::API::POST_CONTENT_TYPE % %(https://tent.io/types/relationship/v0#))}}
+            'Content-Type' => %r{\A#{Regexp.escape(TentD::API::POST_CONTENT_TYPE % %(https://tent.io/types/relationship/v0#initial))}}
           )
         end.expect_response(:status => 200, :schema => :data) do
           expect_schema(:post, '/post')
           expect_headers(
-            'Content-Type' => TentD::API::POST_CONTENT_TYPE % %(https://tent.io/types/relationship/v0#)
+            'Content-Type' => TentD::API::POST_CONTENT_TYPE % %(https://tent.io/types/relationship/v0#initial)
           )
         end
 
         ##
-        # Setup async request expectation for status# post
-        expect_async_request(
-          :method => "PUT",
-          :url => %r{\A#{Regexp.escape(get(:user).entity)}},
-          :path => %r{\A/posts/#{Regexp.escape(URI.encode_www_form_component(TentValidator.remote_entity_uri))}/[^/]+\Z}
-        ) do
-          expect_schema(:post)
-          expect_headers(
-            'Content-Type' => %r{\brel=['"]#{Regexp.escape("https://tent.io/rels/notification")}['"]}
-          )
-          expect_headers(
-            'Content-Type' => %r{\A#{Regexp.escape(TentD::API::POST_CONTENT_TYPE % get(:subscription_type))}}
-          )
-        end.expect_response(:status => 200, :schema => :data) do
-          expect_schema(:post, '/post')
-          expect_headers(
-            'Content-Type' => TentD::API::POST_CONTENT_TYPE % get(:subscription_type)
-          )
-        end
-
-        ##
-        # Create subscription on local server (should trigger relationship init)
+        # Create subscription on remote server (should trigger relationship init)
         expect_response(:status => 200, :schema => :data) do
           data = {
             :type => %(https://tent.io/types/subscription/v0##{URI.encode_www_form_component(get(:subscription_type_base))}),
-            :mentions => [{ 'entity' => TentValidator.remote_entity_uri }],
+            :mentions => [{ 'entity' => get(:user).entity }],
             :content => {
               :type => get(:subscription_type)
             },
             :permissions => {
-              :entities => [TentValidator.remote_entity_uri]
+              :entities => [get(:user).entity]
             }
           }
-          clients(:app, :server => :local, :user => get(:user)).post.create(data)
-        end
-
-        ##
-        # Create post of type subscribed on remote server (should trigger notification)
-        expect_response(:status => 200, :schema => :data) do
-          data = generate_status_post(is_public=true)
           clients(:app).post.create(data)
         end
 
