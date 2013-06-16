2bd19d117664aedea6d4a8414b6bc574f672b8e8
Jesse Stuart <jesse@jessestuart.ca>
2013-06-16 16:07:49 -0400

    Add relationship validation for triggering relationship via mention

--- lib/tent-validator/validators/relationship_validator.rb
+++ lib/tent-validator/validators/relationship_validator.rb
@@ -189,6 +189,70 @@ module TentValidator
       end
     end
 
+    describe "Relationship initialization" do
+      context "create via mention" do
+        set(:user, TentD::Model::User.generate)
+
+        ##
+        # Setup asyc request expectation for relationship#initial post
+        expect_async_request(
+          :method => "PUT",
+          :url => %r{\A#{Regexp.escape(get(:user).entity)}},
+          :path => %r{\A/posts/#{Regexp.escape(URI.encode_www_form_component(TentValidator.remote_entity_uri))}/[^/]+\Z}
+        ) do
+          expect_schema(:post)
+          expect_headers(
+            'Content-Type' => %r{\brel=['"]#{Regexp.escape("https://tent.io/rels/notification")}['"]}
+          )
+          expect_headers(
+            'Content-Type' => %r{\A#{Regexp.escape(TentD::API::POST_CONTENT_TYPE % %(https://tent.io/types/relationship/v0#initial))}}
+          )
+        end.expect_response(:status => 200, :schema => :data) do
+          expect_schema(:post, '/post')
+          expect_headers(
+            'Content-Type' => TentD::API::POST_CONTENT_TYPE % %(https://tent.io/types/relationship/v0#initial)
+          )
+        end
+
+        ##
+        # Setup asyc request expectation for post notification
+        expect_async_request(
+          :method => "PUT",
+          :url => %r{\A#{Regexp.escape(get(:user).entity)}},
+          :path => %r{\A/posts/#{Regexp.escape(URI.encode_www_form_component(TentValidator.remote_entity_uri))}/[^/]+\Z}
+        ) do
+          expect_schema(:post)
+          expect_headers(
+            'Content-Type' => %r{\brel=['"]#{Regexp.escape("https://tent.io/rels/notification")}['"]}
+          )
+          expect_headers(
+            'Content-Type' => %r{\A#{Regexp.escape(TentD::API::POST_CONTENT_TYPE % %(https://types.example.com/fictitious/v0#))}}
+          )
+        end.expect_response(:status => 200, :schema => :data) do
+          expect_schema(:post, '/post')
+          expect_headers(
+            'Content-Type' => TentD::API::POST_CONTENT_TYPE % %(https://types.example.com/fictitious/v0#)
+          )
+        end
+
+        ##
+        # Create post on remote server mentioning local server
+        expect_response(:status => 200, :schema => :data) do
+          data = {
+            :type => %(https://types.example.com/fictitious/v0#),
+            :mentions => [{ 'entity' => get(:user).entity }],
+            :content => {
+              :text => "Hello #{get(:user).entity}!"
+            },
+            :permissions => {
+              :entities => [get(:user).entity]
+            }
+          }
+          clients(:app).post.create(data)
+        end
+      end
+    end
+
   end
 
   TentValidator.validators << RelationshipValidator
