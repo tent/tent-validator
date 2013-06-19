3f755ea8a02c9b12974a5a1fea8c3859a9f975b3
Jesse Stuart <jesse@jessestuart.ca>
2013-06-19 13:55:04 -0400

    Fix CORS validations

--- lib/tent-validator/validators/cors_validator.rb
+++ lib/tent-validator/validators/cors_validator.rb
@@ -34,26 +34,32 @@ module TentValidator
         end
       end
 
-      %w( GET HEAD POST PUT PATCH DELETE ).each do |method|
+      get(:methods).each do |method|
         context "#{method}" do
-          expect_response do
-            expect_headers('Access-Control-Allow-Origin' => /\A.+\Z/)
+          context "when invalid" do
+            expect_response do
+              expect_headers('Access-Control-Allow-Origin' => /\A.+\Z/)
 
-            expect_headers('Access-Control-Expose-Headers' => proc { |response|
-              header = response.headers['Access-Control-Allow-Headers']
-              actual = header.to_s.split(/\s*,\s*/)
-              expected = %w( Server-Authorization Link Count ETag WWW-Authenticate )
+              expect_headers('Access-Control-Expose-Headers' => proc { |response|
+                header = response.headers['Access-Control-Expose-Headers']
+                actual = header.to_s.split(/\s*,\s*/)
+                expected = %w( Server-Authorization Link Count ETag WWW-Authenticate )
 
-              if expected.any? { |v| !actual.include?(v) }
-                (actual | expected).compact.join(', ')
-              else
-                header
+                if expected.any? { |v| !actual.include?(v) }
+                  (actual | expected).compact.join(', ')
+                else
+                  header
+                end
+              })
+
+              clients(:no_auth).http.send(method.downcase, get(:url), get(:params)) do |request|
+                request.headers['Origin'] = 'http://example.com'
               end
-            })
-
-            clients(:no_auth).http.send(method.downcase, get(:url), get(:params)) do |request|
-              request.headers['Origin'] = 'http://example.com'
             end
+          end
+
+          context "when valid" do
+            # TODO
           end
         end
       end
@@ -63,6 +69,7 @@ module TentValidator
       describe "post" do
         set(:url, :post)
         set(:params, :post => 'fake-post-id', :entity => 'fake-entity')
+        set(:methods, %w( GET HEAD PUT ))
 
         behaves_as(:cors)
       end
@@ -70,6 +77,7 @@ module TentValidator
       describe "posts_feed" do
         set(:url, :posts_feed)
         set(:params)
+        set(:methods, %w( GET HEAD ))
 
         behaves_as(:cors)
       end
@@ -77,6 +85,7 @@ module TentValidator
       describe "post_attachment" do
         set(:url, :post)
         set(:params, :post => 'fake-post-id', :entity => 'fake-entity', :name => 'attachment-name')
+        set(:methods, %w( GET HEAD ))
 
         behaves_as(:cors)
       end
@@ -84,30 +93,35 @@ module TentValidator
       describe "attachment" do
         set(:url, :post)
         set(:params, :entity => 'fake-entity', :digest => 'attachment-digest')
+        set(:methods, %w( GET HEAD ))
 
         behaves_as(:cors)
       end
 
       describe "oauth_token" do
         set(:url, :oauth_token)
+        set(:methods, %w( POST ))
 
         behaves_as(:cors)
       end
 
       describe "new_post" do
         set(:url, :new_post)
+        set(:methods, %w( POST ))
 
         behaves_as(:cors)
       end
 
       describe "server_info" do
         set(:url, :server_info)
+        set(:methods, %w( GET HEAD ))
 
         behaves_as(:cors)
       end
 
       describe "batch" do
         set(:url, :batch)
+        set(:methods, %w( GET HEAD ))
 
         behaves_as(:cors)
       end
