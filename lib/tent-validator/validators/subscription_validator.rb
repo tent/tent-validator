b6a09e83f2dcb71b7bd2818f2550132e891b8bb6
Jesse Stuart <jesse@jessestuart.ca>
2013-06-30 14:53:44 -0400

    Update create subscription validation to create the same subscription for multiple entities

--- lib/tent-validator/validators/subscription_validator.rb
+++ lib/tent-validator/validators/subscription_validator.rb
@@ -10,69 +10,79 @@ module TentValidator
     describe "Create Subscription" do
       context "when no existing relationship" do
 
-        set(:user, TentD::Model::User.generate)
+        shared_example :create_relationship_and_subscription do
+          ##
+          # Setup asyc request expectation for relationship#initial post
+          expect_async_request(
+            :method => "PUT",
+            :url => %r{\A#{Regexp.escape(get(:user).entity)}},
+            :path => %r{\A/posts/#{Regexp.escape(URI.encode_www_form_component(TentValidator.remote_entity_uri))}/[^/]+\Z}
+          ) do
+            expect_schema(:post)
+            expect_headers(
+              'Content-Type' => %r{\brel=['"]#{Regexp.escape("https://tent.io/rels/notification")}['"]}
+            )
+            expect_headers(
+              'Content-Type' => %r{\A#{Regexp.escape(TentD::API::POST_CONTENT_TYPE % %(https://tent.io/types/relationship/v0#initial))}}
+            )
+          end.expect_response(:status => 200, :schema => :data) do
+            expect_schema(:post, '/post')
+            expect_headers(
+              'Content-Type' => TentD::API::POST_CONTENT_TYPE % %(https://tent.io/types/relationship/v0#initial)
+            )
+          end
+
+          ##
+          # Setup asyc request expectation for subscription
+          expect_async_request(
+            :method => "PUT",
+            :url => %r{\A#{Regexp.escape(get(:user).entity)}},
+            :path => %r{\A/posts/#{Regexp.escape(URI.encode_www_form_component(TentValidator.remote_entity_uri))}/[^/]+\Z}
+          ) do
+            expect_schema(:post)
+            expect_headers(
+              'Content-Type' => %r{\brel=['"]#{Regexp.escape("https://tent.io/rels/notification")}['"]}
+            )
+            expect_headers(
+              'Content-Type' => %r{\A#{Regexp.escape(TentD::API::POST_CONTENT_TYPE % get(:subscription_post_type))}}
+            )
+          end.expect_response(:status => 200, :schema => :data) do
+            expect_schema(:post, '/post')
+            expect_headers(
+              'Content-Type' => TentD::API::POST_CONTENT_TYPE % get(:subscription_post_type)
+            )
+          end
+
+          ##
+          # Create subscription on remote server (should trigger relationship init and send us the subscription)
+          expect_response(:status => 200, :schema => :data) do
+            data = {
+              :type => get(:subscription_post_type),
+              :mentions => [{ 'entity' => get(:user).entity }],
+              :content => {
+                :type => get(:subscription_type)
+              },
+              :permissions => {
+                :entities => [get(:user).entity]
+              }
+            }
+            clients(:app_auth).post.create(data)
+          end
+        end
+
         set(:subscription_type, %(https://tent.io/types/status/v0#))
         set(:subscription_type_base, %(https://tent.io/types/status/v0))
         set(:subscription_post_type, TentClient::TentType.new("https://tent.io/types/subscription/v0##{get(:subscription_type_base)}").to_s)
 
-        ##
-        # Setup asyc request expectation for relationship#initial post
-        expect_async_request(
-          :method => "PUT",
-          :url => %r{\A#{Regexp.escape(get(:user).entity)}},
-          :path => %r{\A/posts/#{Regexp.escape(URI.encode_www_form_component(TentValidator.remote_entity_uri))}/[^/]+\Z}
-        ) do
-          expect_schema(:post)
-          expect_headers(
-            'Content-Type' => %r{\brel=['"]#{Regexp.escape("https://tent.io/rels/notification")}['"]}
-          )
-          expect_headers(
-            'Content-Type' => %r{\A#{Regexp.escape(TentD::API::POST_CONTENT_TYPE % %(https://tent.io/types/relationship/v0#initial))}}
-          )
-        end.expect_response(:status => 200, :schema => :data) do
-          expect_schema(:post, '/post')
-          expect_headers(
-            'Content-Type' => TentD::API::POST_CONTENT_TYPE % %(https://tent.io/types/relationship/v0#initial)
-          )
+        context "entity A" do
+          set(:user, TentD::Model::User.generate)
+          behaves_as(:create_relationship_and_subscription)
         end
 
-        ##
-        # Setup asyc request expectation for subscription
-        expect_async_request(
-          :method => "PUT",
-          :url => %r{\A#{Regexp.escape(get(:user).entity)}},
-          :path => %r{\A/posts/#{Regexp.escape(URI.encode_www_form_component(TentValidator.remote_entity_uri))}/[^/]+\Z}
-        ) do
-          expect_schema(:post)
-          expect_headers(
-            'Content-Type' => %r{\brel=['"]#{Regexp.escape("https://tent.io/rels/notification")}['"]}
-          )
-          expect_headers(
-            'Content-Type' => %r{\A#{Regexp.escape(TentD::API::POST_CONTENT_TYPE % get(:subscription_post_type))}}
-          )
-        end.expect_response(:status => 200, :schema => :data) do
-          expect_schema(:post, '/post')
-          expect_headers(
-            'Content-Type' => TentD::API::POST_CONTENT_TYPE % get(:subscription_post_type)
-          )
+        context "entity B" do
+          set(:user, TentD::Model::User.generate)
+          behaves_as(:create_relationship_and_subscription)
         end
-
-        ##
-        # Create subscription on remote server (should trigger relationship init and send us the subscription)
-        expect_response(:status => 200, :schema => :data) do
-          data = {
-            :type => get(:subscription_post_type),
-            :mentions => [{ 'entity' => get(:user).entity }],
-            :content => {
-              :type => get(:subscription_type)
-            },
-            :permissions => {
-              :entities => [get(:user).entity]
-            }
-          }
-          clients(:app_auth).post.create(data)
-        end
-
       end
     end
 
